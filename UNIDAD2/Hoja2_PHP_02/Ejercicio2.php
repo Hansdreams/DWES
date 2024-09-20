d75e917d6cd4f0546204b89bb03a92932863d4a7
Hansdreams <gustavorodrigo.bautistapocohuanca@iesmiguelherrero.com>
2024-09-20 20:15:56 +0200

    Hojas 1 y 2

---
+++ UNIDAD2/Hoja2_PHP_02/Ejercicio2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ejercicio2</title>
+    </head>
+    <body>
+        
+
+        <?php
+
+            $nombre = 'Gustavo';//varaible de texto 
+            $edad = 28;//variable numerica
+
+            echo 'Segundo ejercicio: visualización del contenido de variables';
+            
+            echo "<br>Mi nombre es $nombre y mi edad es $edad";
+            
+        ?>
+
+    </body>
+<html>
---
+++ UNIDAD2/Hoja2_PHP_02/Ejercicio3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ejercicio3</title>
+    </head>
+    <body>
+        
+
+        <?php
+
+            $operador1 = 13;
+            $operador2 = 4;
+
+            $resultado = 13 - 4;
+
+            echo "$operador1 - $operador2 = $resultado <br>";
+
+            $resultado = 13 + 4;
+
+            echo "$resultado";
+            
+        ?>
+
+    </body>
+<html>
